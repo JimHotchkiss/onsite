7b51a26a198a772a475fae2c993c2f9a8b7a80ac
jimhotchkiss <jimbohtochkiss@yahoo.com>
2018-06-12 14:20:15 -0700

    Added corrections to _form, _comment and comment delete.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,12 @@ class CommentsController < ApplicationController
     @comment = @issue.comments.create(params[:comment].permit(:body))
     redirect_to issue_path(@issue)
   end
+
+  def destroy
+   find_issue
+   @comment = find_issue.comments.find(params[:id])
+   @comment.destroy
+
+   redirect_to issue_path(@issue)
+ end
 end
